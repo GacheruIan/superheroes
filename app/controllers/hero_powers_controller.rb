47401874e649287d0cb4db406709419c249acd34
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-12-05 09:19:21 +0300

    modify files

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -4,14 +4,13 @@ skip_before_action :verify_authenticity_token
 
     def index
         hero_powers = HeroPower.all
-        render json: hero_powers, except:[:created_at, :updated_at]
+        render json: hero_powers, status: :ok
     end
 
-
     def create
-        hero_powers = HeroPower.create!(hero_params)
+        hero_powers = HeroPower.create(hero_params)
         if hero_powers.valid?
-        render json: hero_powers, status: :created  
+        render json: hero_powers, status: :created
         else
         render json: {errors: hero_powers.errors}, status: :unprocessable_entity    
         end
--- app/serializers/hero_power_serializer.rb
+++ app/serializers/hero_power_serializer.rb
@@ -1,4 +1,7 @@
 class HeroPowerSerializer < ActiveModel::Serializer
-
   attributes  :strength, :power_id, :hero_id
+
+  belongs_to :hero
+  belongs_to :power
+
 end
